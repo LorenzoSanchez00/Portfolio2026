349f64b914ef77ca035f5e1b0432412668829c28
LorenzDev <lorenzosanchez.9810@gmail.com>
2026-06-20 12:55:02 -0300

    generando componente info

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
+import { InfoComponent } from './components/info/info.component';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkActive } from '@angular/router';
+import { NavComponent } from "./components/nav/nav.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, InfoComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/info/info.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info',
+  imports: [],
+  templateUrl: './info.component.html',
+  styleUrl: './info.component.css',
+})
+export class InfoComponent {}
