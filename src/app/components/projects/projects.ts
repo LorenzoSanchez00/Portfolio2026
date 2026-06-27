1bde8b4d8112dc209811663273cf9620e68a17c9
LorenzDev <lorenzosanchez.9810@gmail.com>
2026-06-27 19:07:55 -0300

    agregando componente projects

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -1,4 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+export class stack{
+  name: string;
+  constructor(technology: string){
+    this.name = technology;
+  }
+}
+export interface MyProject{
+  name: string;
+  description: string;
+  img: string;
+  stack: stack[];
+  link: string;
+}
 
 @Component({
   selector: 'app-projects',
@@ -6,4 +20,30 @@ import { Component } from '@angular/core';
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
-export class Projects {}
+export class Projects {
+  projects = signal<MyProject[]>([
+    {name:'Gestor de Finanzas',
+     description:'Sistema de gestión de finanzas personales creado con tecnologías .NET, permite al usuario registrar ingresos y egresos, organizarlos por categorías y cuentas, visualizarlos en tablas o calendario, y obtener reportes en Excel.',
+     img:'imgs/project-GestionFinanzas.png',
+     stack:[{name:'ASP.NET'},
+            {name:'C#'},
+            {name:'razor'},
+            {name:'Bootstrap'},
+            {name:'JQuery'},
+            {name:'Dapper'},
+            {name:'SQLServer'}],
+      link:'--'},
+
+    {name:'Landing Page Ecommerce', 
+     description:'Landing Page para un reconocido mentor y creador de contenidos sobre ecommerce.',
+     img:'proximamente',
+     stack:[{name:'HTML'},
+            {name:'CSS'},
+            {name:'Bootstrap'},
+            {name:'Typescript'},
+            {name:'Angular'},
+            {name:'Figma'},],
+      link:'--'}
+  ])
+
+}
