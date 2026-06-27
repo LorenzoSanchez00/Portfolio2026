b06bf39952863b84b9241f68aa1201b464962000
LorenzDev <lorenzosanchez.9810@gmail.com>
2026-06-27 15:37:57 -0300

    agregando componente projects

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLinkActive } from '@angular/router';
 import { NavComponent } from "./components/nav/nav.component";
 import { Skills } from './components/skills/skills';
+import { Projects } from "./components/projects/projects";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, InfoComponent, Skills],
+  imports: [RouterOutlet, NavComponent, InfoComponent, Skills, Projects],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
