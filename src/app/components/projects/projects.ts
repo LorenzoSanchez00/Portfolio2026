83512979b7fe6b0da821982b8efcfab0a44036ff
LorenzDev <lorenzosanchez.9810@gmail.com>
2026-07-04 17:54:51 -0300

    agregando img de proyecto y mejorando estilos de projects

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -24,7 +24,7 @@ export class Projects {
   projects = signal<MyProject[]>([
     {name:'Gestor de Finanzas',
      description:'Sistema de gestión de finanzas personales creado con tecnologías .NET, permite al usuario registrar ingresos y egresos, organizarlos por categorías y cuentas, visualizarlos en tablas o calendario, y obtener reportes en Excel.',
-     img:'imgs/project-GestionFinanzas.png',
+     img:'imgs/project1.png',
      stack:[{name:'ASP.NET'},
             {name:'C#'},
             {name:'razor'},
@@ -32,18 +32,30 @@ export class Projects {
             {name:'JQuery'},
             {name:'Dapper'},
             {name:'SQLServer'}],
-      link:'--'},
+      link:'#'},
+
+    {name:'App de traduccion de audios', 
+     description:'El usuario puede grabar un audio en español y la aplicación lo traduce a inglés, portugués francés o japonés. Aplicación web hecha en ASP.NET Core MVC que permite grabar la voz del usuario desde el navegador, transcribirla, traducirla a otro idioma y generar un nuevo audio con esa traducción usando ElevenLabs.',
+     img:'imgs/project2.jpeg',
+     stack:[{name:'HTML'},
+            {name:'CSS'},
+            {name:'Bootstrap'},
+            {name:'ASP.NET Core MVC'},
+            {name:'C#'},
+            {name:'Javascript'},
+            {name:'ElevenLabs'},],
+      link:'https://github.com/LorenzoSanchez00/AudioTranslator'},
 
     {name:'Landing Page Ecommerce', 
      description:'Landing Page para un reconocido mentor y creador de contenidos sobre ecommerce.',
-     img:'proximamente',
+     img:'/imgs/project3.jpeg',
      stack:[{name:'HTML'},
             {name:'CSS'},
             {name:'Bootstrap'},
             {name:'Typescript'},
             {name:'Angular'},
             {name:'Figma'},],
-      link:'--'}
+      link:'#'}
   ])
 
 }
