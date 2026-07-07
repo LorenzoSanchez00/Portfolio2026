56575b7a368897a22cd78e856632641cd6accf43
LorenzDev <lorenzosanchez.9810@gmail.com>
2026-07-07 15:31:58 -0300

    agregando componente mail-form

---
+++ src/app/components/mail-form/mail-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mail-form',
+  imports: [],
+  templateUrl: './mail-form.html',
+  styleUrl: './mail-form.css',
+})
+export class MailForm {}
