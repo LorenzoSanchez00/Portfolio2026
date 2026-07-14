538309b36bc01f5e0e3e5defcf061970f884e88a
LorenzDev <lorenzosanchez.9810@gmail.com>
2026-07-14 20:49:30 -0300

    agregando estilos al footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,11 +5,12 @@ import { NavComponent } from "./components/nav/nav.component";
 import { Skills } from './components/skills/skills';
 import { Projects } from "./components/projects/projects";
 import { MailForm } from "./components/mail-form/mail-form";
+import { Footer } from "./components/footer/footer";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, InfoComponent, Skills, Projects, MailForm],
+  imports: [RouterOutlet, NavComponent, InfoComponent, Skills, Projects, MailForm, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
