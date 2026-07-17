925c5f884f4af9b121f1c2c97e6782f9eb1171ad
LorenzDev <lorenzosanchez.9810@gmail.com>
2026-07-17 08:35:33 -0300

    agregando la ruta del repo

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import { InfoComponent } from './components/info/info.component';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLinkActive } from '@angular/router';
 import { NavComponent } from "./components/nav/nav.component";
-import { Skills } from './components/skills/skills';
+import { Skills } from "./components/skills/skills";
 import { Projects } from "./components/projects/projects";
 import { MailForm } from "./components/mail-form/mail-form";
 import { Footer } from "./components/footer/footer";
